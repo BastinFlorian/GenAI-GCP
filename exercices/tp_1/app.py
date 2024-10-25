70964d9c1dc322718cb2d8201292f14040d342cf
Mehyar Mlaweh <mehyarmlaweh0@gmail.com>
2024-10-25 11:01:54 +0100

    hello Tp3

--- exercices/tp_1/app.py
+++ exercices/tp_1/app.py
@@ -1,8 +1,18 @@
-"""Streamlit app"""
 import streamlit as st
 
+st.sidebar.title('Settings')
+language = st.sidebar.selectbox('Language', ['English', 'French'])
+gender = st.sidebar.selectbox('Gender', ['Man', 'Woman'])
 
-st.title('Hello, Streamlit!')
-message = st.text_input('Say something')
-if message:
-    st.write(f'You said: {message}')
+name = st.text_input("What's your name?")
+
+if name:
+    if language == 'English' and gender == 'Man':
+        st.write(f'Hello Mr. {name}')
+    elif language == 'English' and gender == 'Woman':
+
+        st.write(f'Hello Ms. {name}')
+    elif language == 'French' and gender == 'Man':
+        st.write(f'Bonjour monsieur {name}')
+    elif language == 'French' and gender == 'Woman':
+        st.write(f'Bonjour madame {name}')
--- exercices/tp_2/app.py
+++ exercices/tp_2/app.py
@@ -1,21 +1,40 @@
-"""Streamlit app"""
 import streamlit as st
 import requests
 
-# TODO
-# HOST = "http://[container_name]:[host]/answer"
+#HOST = "http://fastapi-container:8181/answer"
+HOST = "https://mehyar-fastapi-1021317796643.europe-west1.run.app/answer"
 
-st.title('Hello, Streamlit!')
-message = st.text_input('Say something')
-if message:
-    # TODO
-    response = requests.post(
-        HOST,
-        ...
-        json=...,
-        timeout=20
-    )
-    if response.status_code == 200:
-        st.write(response.json()["message"])
+
+# Streamlit app setup
+st.title('Hello!')
+st.write("Enter your details to receive a personalized message.")
+
+# Input fields
+name = st.text_input('Name:')
+genre = st.selectbox('Genre:', ['male', 'female'])  
+language = st.text_input('Preferred Language:')
+
+# Submit buttonw
+if st.button('Submit'):
+    if name and genre and language:
+        # Prepare the JSON payload
+        payload = {
+            "name": name,
+            "genre": genre,
+            "language": language
+        }
+
+        #  POST to FastAPI
+        try:
+            response = requests.post(HOST, json=payload, timeout=20)
+
+            if response.status_code == 200:
+                # Display the message from the FastAPI response
+                st.write(response.json()["message"])
+            else:
+                st.write(f"Error: {response.status_code} - Unable to get a response from the API")
+        
+        except requests.exceptions.RequestException as e:
+            st.write(f"Error: {e}")
     else:
-        st.write("Error: Unable to get a response from the API")
+        st.write("Please fill in all the fields.")
