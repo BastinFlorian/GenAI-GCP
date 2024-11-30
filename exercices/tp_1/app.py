9fb968d5a3ac44df2fbeec3a4e72a4aaf438a1ac
AzizMarouani123 <167762031+AzizMarouani123@users.noreply.github.com>
2024-11-30 11:20:44 +0100

    Le code final de app.py de tp1

--- exercices/tp_1/app.py
+++ exercices/tp_1/app.py
@@ -1,8 +1,30 @@
 """Streamlit app"""
-import streamlit as st
+import streamlit as st 
 
 
 st.title('Hello, Streamlit!')
-message = st.text_input('Say something')
-if message:
-    st.write(f'You said: {message}')
+
+
+
+# Barre latérale pour les choix
+st.sidebar.header("Settings")
+
+# Sélection de la langue
+language = st.sidebar.selectbox("Language", ["English", "French"])
+
+# Sélection du genre
+gender = st.sidebar.selectbox("Gender", ["Man", "Woman"])
+
+# Demande du nom de la personne
+name = st.text_input("What is your name?")
+
+# Message personnalisé en fonction de la langue et du genre
+if name:
+    if language == "English" and gender == "Man":
+        st.write(f"Hello Mr. {name}")
+    elif language == "French" and gender == "Woman":
+        st.write(f"Bonjour madame {name}")
+    elif language == "English" and gender == "Woman":
+        st.write(f"Hello Mrs. {name}")
+    elif language == "French" and gender == "Man":
+        st.write(f"Bonjour monsieur {name}")
