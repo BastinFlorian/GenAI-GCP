523ba6c11fefbc4a20eec8428469163846c19ee3
AzizMarouani123 <167762031+AzizMarouani123@users.noreply.github.com>
2024-11-30 13:13:25 +0100

    Update api.py de tp2

--- tp_2/api.py
+++ tp_2/api.py
@@ -1,31 +1,19 @@
-"""API"""
+# api.py
+
+
 from fastapi import FastAPI
 from pydantic import BaseModel
-
-
 app = FastAPI()
 
 
 class UserInput(BaseModel):
-    """
-    UserInput is a data model representing user input.
-
-    Attributes:
-        name (str): The name of the user.
-        genre (str): The genre preference of the user.
-        language (str): The language preference of the user.
-    """
-    # TODO fill the required attributes
+    name: str
+    genre: str
+    language: str
 
 
 @app.post("/answer")
 def answer(user_input: UserInput):
-    """
-    Generates a greeting message based on the user's input.
-    Args:
-        user_input (UserInput): An object containing user details such as name, genre, and language.
-    Returns:
-        dict: A dictionary containing a greeting message.
-    """
-    # TODO edit the implementation to match the required ouput from #1
-    return {"message": f"Hello Mr {user_input.name} from {user_input.genre}, {user_input.language}"}
+    greeting = f"Bonjour {'monsieur' if user_input.genre.lower() == 'male' else 'madame'} {user_input.name}"
+    return {"message": greeting}if user_input.genre.lower() == 'male' else 'madame'} {user_input.name}"
+    return {"message": greeting}
