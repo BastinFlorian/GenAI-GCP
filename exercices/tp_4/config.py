2fb2f1484e7f4c2bc9b3b2a0e396fd6c47e1b5f5
Guembri01 <100989581+Guembri01@users.noreply.github.com>
2024-12-12 19:27:22 +0100

    Update config.py

--- exercices/tp_4/config.py
+++ exercices/tp_4/config.py
@@ -1,13 +1,13 @@
 """Configuration file for project settings."""
 
-PROJECT_ID = "dauphine-437611"
-REGION = "europe-west1"  # Cloud SQL
-INSTANCE = "gen-ai-instance"
-DATABASE = "gen_ai_db"
-TABLE_NAME = "gen-ai-table"
-DB_USER = "students"  # Cloud Storage
-BUCKET_NAME = "dauphine-bucket"
-TABLE_NAME = "fb_table"
-GOOGLE_API_KEY = "AIzaSyBJ2fcbENlNXhQx0EVT1o5GCkuNpcgRCQE"
-DB_PASSWORD = "|Q46Tr^tTqB8hSpO"
+PROJECT_ID = "..."
+REGION = "..."  # Cloud SQL
+INSTANCE = "..."
+DATABASE = "..."
+TABLE_NAME = "..."
+DB_USER = "..." # Cloud Storage
+BUCKET_NAME = "..."
+TABLE_NAME = "..."
+GOOGLE_API_KEY = "..."
+DB_PASSWORD ="..."
 DOWNLOADED_LOCAL_DIRECTORY = "./downloaded_files"
